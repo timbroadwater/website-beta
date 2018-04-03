a16a5c7d4af7ad5aae3c1038463bbce25d9f511f
Tim Broadwater <timbrdh2o@gmail.com>
2018-04-02 20:02:04 -0400

    Terms

---
+++ agreements/index.php
@@ -0,0 +1,85 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<?php include '../imports/head.html';?>
+	</head>
+	<body>
+		<header id="header" class="header">
+			<?php include '../imports/header.html';?>
+		</header>
+		<section id="hero">
+			<div class="banner-wrapper">
+				<h2>Privacy Policy & Terms of Use</h2>
+			</div>
+		</section>
+		<section id="process">
+			<div class="section-wrapper2">										<h2>Privacy Policy</h2>
+				<p>We reserve the right to update the Website and these Terms of Use from time to time, at our discretion and without notice. As a user of this website, it is your responsibility to check this page for changes.</p>
+				<p>Unless otherwise stated, User Goodness LLC (referred to here forth as either “User Goodness LLC,” “we,” “our” or “us”) and/or its licensors own the intellectual property rights in the website and material on the website. Subject to the license below, all these intellectual property rights are reserved.</p>
+				<p>You may view, download for caching purposes only, and print pages from the website for your own personal use, subject to the restrictions set out below and elsewhere in these terms and conditions.</p>
+				<p>You must not: republish material from this website (including republication on another website); sell, rent or sub-license material from the website; reproduce, duplicate, copy or otherwise exploit material on this website for a commercial purpose; edit or otherwise modify any material on the website; or redistribute material from this website, except for content specifically and expressly made available for redistribution.</p>
+				<p>You must not use this website in any way that causes, or may cause, damage to the website or impairment of the availability or accessibility of the website; or in any way which is unlawful, illegal, fraudulent or harmful, or in connection with any unlawful, illegal, fraudulent or harmful purpose or activity.</p>
+				<p>You must not use this website to copy, store, host, transmit, send, use, publish or distribute any material which consists of (or is linked to) any spyware, computer virus, Trojan horse, worm, keystroke logger, rootkit or other malicious computer software.</p>
+				<p>You must not conduct any systematic or automated data collection activities (including without limitation scraping, data mining, data extraction and data harvesting) on or in relation to this website without our expressly written consent.</p>
+				<p>You must not use this website to transmit or send unsolicited commercial communications.</p>
+				<p>You must not use this website for any purposes related to marketing without our expressly written consent.</p>
+				<p>Access to certain areas of this website is restricted. We reserve the right to restrict access to premium content areas of this website, or indeed this entire website, at our own discretion and without notice.</p>
+				<p>If we provide you with a user ID and password to enable you to access restricted areas of this website or other content or services, you must ensure that the user ID and password are kept confidential.</p>
+				<p>We may disable your user ID and password in our sole discretion without notice or explanation.</p>
+				<p>In these terms and conditions, “your user content” means material (including without limitation text, images, audio material, video material and audio-visual material) that you submit to this website, for whatever purpose.</p>
+				<p>You grant to us a worldwide, irrevocable, non-exclusive, royalty-free license to use, reproduce, adapt, publish, translate and distribute your user content in any existing or future media. You also grant us  the right to sub-license these rights, and the right to bring an action for infringement of these rights.</p>
+				<p>Your user content must not be illegal or unlawful, must not infringe any third party’s legal rights, and must not be capable of giving rise to legal action whether against you or us or a third party (in each case under any applicable law).</p>
+				<p>You must not submit any user content to the website that is or has ever been the subject of any threatened or actual legal proceedings or other similar complaint.</p>
+				<p>We reserves the right to edit or remove any material submitted to this website, or stored on our servers, or hosted or published upon this website.</p>
+				<p>Notwithstanding our rights under these terms and conditions in relation to user content, we do not undertake to monitor the submission of such content to, or the publication of such content on, this website.</p>
+				<p>This website is provided “as is” without any representations or warranties, express or implied. We make no representations or warranties in relation to this website or the information and materials provided on this website.
+				Without prejudice to the generality of the foregoing paragraph, we do not warrant that: this website will be constantly available, or available at all; or the information on this website is complete, true, accurate or non-misleading.</p>
+				<p>We will not be liable to you (whether under the law of contact, the law of torts or otherwise) in relation to the contents of, or use of, or otherwise in connection with, this website: to the extent that the website is provided free-of-charge, for any direct loss; for any indirect, special or consequential loss; or for any business losses, loss of revenue, income, profits or anticipated savings, loss of contracts or business relationships, loss of reputation or goodwill, or loss or corruption of information or data.</p>
+				<p>These limitations of liability apply even if we have been expressly advised of the potential loss.</p>
+				<p>You accept that, as a limited liability entity, we have an interest in limiting the personal liability of its officers and employees. You agree that you will not bring any claim personally against our officers or employees in respect of any losses you suffer in connection with the website.</p>
+				<p>Without prejudice to the foregoing paragraph, you agree that the limitations of warranties and liability set out in this website disclaimer will protect our officers, employees, agents, subsidiaries, successors, assigns and sub-contractors.</p>
+				<p>If any provision of this website disclaimer is, or is found to be, unenforceable under applicable law, that will not affect the enforceability of the other provisions of this website disclaimer.</p>
+				<p>You hereby indemnify us and undertake to keep us indemnified against any losses, damages, costs, liabilities and expenses (including without limitation legal expenses and any amounts paid by us to a third party in settlement of a claim or dispute on the advice of our legal advisers) incurred or suffered by us arising out of any breach by you of any provision of these terms and conditions.</p>
+				<p>Without prejudice to our other rights under these terms and conditions, if you breach these terms and conditions in any way, we may take such action as what we deem appropriate to deal with the breach, including suspending your access to the website, prohibiting you from accessing the website, blocking computers using your IP address from accessing the website, contacting your internet service provider to request that they block your access to the website and/or bringing court proceedings against you.</p>
+				<p>These terms and conditions, together with our Privacy Policy, constitute the entire agreement between you and User Goodness LLC in relation to your use of this website, and supersede all previous agreements in respect of your use of this website.</p>
+				<p>These terms and conditions will be governed by and construed in accordance with the Commonwealth of Pennsylvania, and any disputes relating to these terms and conditions will be subject to non-exclusive jurisdiction.</p>
+			</div>
+		</section>
+		<section class="greybg">
+			<div class="section-wrapper2">
+				<h2>Terms of Use</h2>
+				<p>We reserve the right to update the Website and this Privacy Policy from time to time, at our discretion and without notice. As a user of this website, it is your responsibility to check our policies for changes.</p>
+				<p>We automatically collect non-personally identifiable information from you when you visit our site.
+				IP Addresses: This includes your Internet Protocol (“IP”) address, which is the number automatically assigned to your computer whenever you access the Internet. This number does not identify your name, email address, or other personal information.</p>
+				<p>Google Analytics: Through Google Analytics, we collect overall demographics and page exploration habits (page views, bounce rate, device). These analytics do not identify your name, email address, or other personal information.
+				Google Analytics is a web analysis service provided by Google. Google utilizes the data collected to track and examine the use of www.example.com, to prepare reports on its activities and share them with other Google services.
+				Google may use the data collected to contextualize and personalize the ads of its own advertising network. Personal data collected: Cookie and Usage Data. Place of processing: USA.</p>
+				<p>We do not automatically collect the email addresses or names of visitors to our site. The only way we obtain personally identifiable information is when you provide it voluntarily by: Filling out a contact form on our contact page, Filling out a form on an eBook download page.</p>
+				<p>Through any of the above forms or requests, visitors may be asked to fill in their name, email address, website, company, etc.</p>
+				<p>Unless provided by you explicitly through form submission, we cannot access any personal information from your visit to our website.</p>
+				<p>The information gathered by our site is used to: Answer consumers’ and professionals’ questions, Improve our website user experience, Determine the number of visitors to our site each day, and from where they are coming.
+				We do not sell, trade or rent personal information about our visitors, nor will we disclose your personal information to organizations unless we are authorized by you to do so, or are required by law.</p>
+				<p>By using our website, you consent to the collection and use of the information described above.</p>
+				<p>Please note that we cannot control the content, availability or privacy practices of sites that link to our sites, or the ability of other sites to link to our sites.</p>
+				<p>We are not responsible for any links to or from other sites. Links to or from a third party are not an endorsement by us unless otherwise expressly stated.</p>
+			</div>
+		</section>
+		<section id="footer">
+			<?php include '../imports/footer.html';?>
+		</section>
+		<section id="quote">
+			<div class="section-wrapper3">
+				<span class="message"></span>
+			</div>
+		</section>
+		
+		<!-- Nav Selection -->
+		<script type="text/javascript">
+			$(function() {
+			  $('.nav-process').addClass('selected');
+			});
+		</script>
+	</body>
+</html>
