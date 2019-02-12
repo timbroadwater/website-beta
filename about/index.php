5c6e7692e42e2f98602ca2ededf27e2ae73e4a56
Tim Broadwater <tim.broadwater@leidos.com>
2019-02-12 15:15:36 -0500

    Updating Members

--- about/index.php
+++ about/index.php
@@ -48,6 +48,7 @@
 						<p>Tim has over 20 years of expertise in UI Design, Front-End Development, UX Research, and content strategy. He is well-versed  working with digital product life cycles, Lean UX principles, and aligning user goals to business goals. Current areas of interest to Tim include Data Visualization, Enterprise Software Usability, Facilitating Design Thinking, and Omnichannel UX. His UX background spans across advertising, alternate fuels, defense, eCommerce, education, healthcare, library sciences, museums, nonprofit, and software development. Some of Tim’s other skills include data crunching, espresso milkshakes, and tattoos.</p>
 					</div>
 				</div>
+				
 				<div class="bio-container">
 					<div class="bio-container-left">
 						<img src="../img/danielledemme.jpg" alt="Danielle Demme" class="bio-container-image"/>
@@ -56,7 +57,8 @@
 					<div class="bio-container-right">
 						<p>Danielle, currently living in Austin, Texas  brings over 2 years of UI/UX experience from eCommerce, healthcare and enterprise software. Other skills include over 6 years of traditional graphic and visual design. She is passionate about solving complex problems with human centered design and believes in meaningful solutions that create an impact. You can find Danielle on two wheels in the city, on a long run, or traveling the world every chance she gets. Engaging with others and traveling provides her with powerful insights and more importantly, unlocks empathy for the people who could be touched by the designs she creates.</p>
 					</div>
-				</div>				
+				</div>		
+
 				<div class="bio-container">
 					<div class="bio-container-left">
 						<img src="../img/denisehardman.jpg" alt="Denise Hardman" class="bio-container-image"/>
@@ -65,7 +67,28 @@
 					<div class="bio-container-right">
 						<p>A native of Northeast Ohio, Denise brings over 7 years of experience in UX. Her UX background spans across eCommerce, cultural arts, non-profits, and business enterprise solutions. As a UX Researcher, Denise focuses on applying methodology for both qualitative and quantitative research methods as well as conducting assessments for products and services. Some of Denise’s other skills include a major obsession with cats, crafting fiber stuff, drinking tea, managing an addiction to online shopping, tabletop and PC gaming, as well as watching bad movies!</p>
 					</div>
-				</div>				
+				</div>	
+
+				<div class="bio-container">
+					<div class="bio-container-left">
+						<img src="../img/aprilleifferhenry.jpg" alt="April Leiffer Henry" class="bio-container-image"/>
+						<h3>April Leiffer Henry</h3>
+					</div>
+					<div class="bio-container-right">
+						<p>April brings eight years of UX experience in education and healthcare. A writer and content strategist, April has worked as a newspaper reporter, public relations specialist and digital strategist. She excels at interviewing and researching, asking just the right questions that lead to high-quality content. April knows how to organize website content in a way that helps users find it and want to read it, and her writing keeps readers engaged and prompts them to take action. When she’s not writing, April is probably playing with her three children or hiding from them while eating dark chocolate. She also enjoys eating the delicious food her husband makes, reading good books and listening to musicals.</p>
+					</div>
+				</div>	
+
+				<div class="bio-container">
+					<div class="bio-container-left">
+						<img src="../img/nickkalpakis.jpg" alt="Nick Kalpakis" class="bio-container-image"/>
+						<h3>Nick Kalpakis</h3>
+					</div>
+					<div class="bio-container-right">
+						<p>Born and raised in Pittsburgh PA, Nicholas has over 4 years experience as a full stack developer as well as studying computer science at the University of Pittsburgh. With a wide range of experience from cyber security to e-commerce, Nicholas has a passion for using his creativity to develop effective solutions that help users everyday.  He also enjoys health & nutrition, sports and enjoying time with friends & family.</p>
+					</div>
+				</div>	
+
 				<div class="bio-container">
 					<div class="bio-container-left">
 						<img src="../img/zackpontious.jpg" alt="Zack Pontious" class="bio-container-image"/>
@@ -75,23 +98,6 @@
 						<p>Zack grew up in Northwestern, Pennsylvania. Zack has over 4 years of experience in UX spanning across eCommerce, and finance and 13 years of experience in fine art and design. Art was always a passion for him which he studied in undergrad and graduate school. He traveled all over the United States in his 1999 Toyota Camry and lived in Salt Lake City and New Orleans along the way. He always considered Western Pennsylvania home and now resides in beautiful Pittsburgh. UX allows Zack to bring many of his passions and skill sets together. He likes to focus design, creative problem solving, and strategic thinking.</p>
 					</div>
 				</div>
-				<div class="bio-container">
-					<div class="bio-container-left">
-						<img src="../img/mschofield.jpg" alt="Michael Schofield" class="bio-container-image"/>
-						<h3>Michael Schofield</h3>
-					</div>
-					<div class="bio-container-right">
-						<p>A Michigander expat in Ft. Lauderdale, Florida, Michael has five years in service and user 
-experience design in libraries and higher education.  As a developer and researcher in 
-these spaces and in enterprise software, he specializes in creating systems for strategic design
-that help leadership estimate how changes to the user experience positively or negatively
-impact the bottom line, as well afford teams the tools for responding to those opportunities 
-with less cost. Michael hosts Metric: The User Experience Design podcast; 
-started LibUX, a community, blahg, and conference
-for folks into library and nonprofit design; and he serves on the Practical Service Design
-leadership team.</p>
-					</div>
-				</div>				
 				<div class="bio-container">
 					<div class="bio-container-left">
 						<img src="../img/kevinvoller.jpg" alt="Kevin Voller" class="bio-container-image"/>
